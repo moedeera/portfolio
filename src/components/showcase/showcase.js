6d178c81eef2660c197a619acb12c8657d6774ff
moedeera <82620264+moedeera@users.noreply.github.com>
2022-06-29 12:12:02 -0600

    commmit

--- src/components/showcase/showcase.js
+++ src/components/showcase/showcase.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./showcase.css";
 import { useEffect, useRef } from "react";
 import pic1 from "./Saly-36.png";
+import pic2 from "./pngegg.png";
 import Lottie from "lottie-web";
 
 export const Showcase = () => {
@@ -27,10 +28,12 @@ export const Showcase = () => {
           <h1>
             Web<span style={{ color: "var(--primary-color)" }}>Design</span>{" "}
           </h1>
-          <h1 style={{ fontWeight: "500" }}>and development</h1>
+          <h1 className="text" style={{ fontWeight: "500" }}>
+            and development
+          </h1>
         </div>
         <div className="showcase-lottie" ref={container}></div>
-        <div className="showcase-cta">
+        <div className="showcase-cta small">
           <button className="btn btn-secondary">
             See Portfolio{" "}
             <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
@@ -39,6 +42,15 @@ export const Showcase = () => {
             Contact <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
           </button>
         </div>
+      </div>
+      <div className="showcase-cta large">
+        <button className="btn btn-secondary">
+          See Portfolio{" "}
+          <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
+        </button>
+        <button className="btn btn-alternate">
+          Contact <i class="fa fa-chevron-circle-down" aria-hidden="true"></i>
+        </button>
       </div>
     </div>
   );
