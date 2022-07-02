9061dcaf7410c70176d9ab6f1ba47a412f52ed1c
moedeera <82620264+moedeera@users.noreply.github.com>
2022-07-01 20:49:49 -0600

    committ

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Homepage } from "./pages/Homepage/Homepage";
+import { Homepage } from "./pages/homepage/Homepage";
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
 import { PortfolioPage } from "./pages/Portfolio/Portfolio";
