bde126b32a51e6124b8d85239dc3d955c6735b14
moedeera <82620264+moedeera@users.noreply.github.com>
2022-07-05 23:09:11 -0600

    committ

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Contact.css";
 import pic1 from "./saly-28.png";
+import { PageHeader } from "../../components/PageHeader/PageHeader";
 
 export const Contact = () => {
   return (
     <div className="contact-page">
+      <div className="contact-header-container">
+        <PageHeader tagline={"Contact"} header={"Get In touch"} />
+      </div>
       <div className="contact-page-container">
         <div className="contact-form">
           <div className="contact-form-text">
-            <h3>Get in Touch</h3>
+            <h3>
+              Send me any inquiries regarding services or potential employment
+            </h3>
             <p>
               Need to contact me for a a job or employment purposes, just send
               me a quick message and I should respond to you as soon as
@@ -17,18 +23,14 @@ export const Contact = () => {
           </div>
           <div className="contact-form-box">
             <div className="cf-form">
-              <h3> Send a message</h3>
-              <input placeholder="name" />
-              <input placeholder="name" />
-              <textarea placeholder="name" />
-              <button>Send Message</button>
+              <h1> Message Me</h1>
+              <input placeholder="Enter Name" />
+              <input placeholder="Enter Email" />
+              <textarea placeholder="Enter Message" />
+              <button className="btn btn-alternate">Send Message</button>
             </div>
             <div className="cf-image">
-              <img
-                src={pic1}
-                alt=""
-                style={{ width: "100%", maxWidth: "200px" }}
-              />
+              <img src={pic1} alt="" />
             </div>
           </div>
         </div>
