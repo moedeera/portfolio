25e991282511af498590b6c28fff83b39794720c
moedeera <82620264+moedeera@users.noreply.github.com>
2022-07-05 21:13:56 -0600

    committ

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -26,10 +26,16 @@ export const About = () => {
               the 1500s, when an unknown printer took a galley of type and
               scrambled it to make a type specimen book. It has survived not
               only five centuries, but also the leap into electronic.
+              <br />
+              is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not
+              only five centuries, but also the leap into electronic.
             </p>
           </div>
         </Fade>
-        <Fade up>
+        {/* <Fade up>
           <div className="about-images">
             <img
               src={
@@ -37,17 +43,21 @@ export const About = () => {
               }
               alt=""
             />
-            <img
-              className="about-img-2"
-              src={
-                "https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-              }
-              alt=""
-            />
           </div>
-        </Fade>
+        </Fade> */}
         <Fade>
           <div className="about-skills">
+            <div className="skills-grid">
+              <i className="fa fa-jsfiddle"></i>
+              <i className="fa fa-css3"></i>
+              <i class="fa fa-github" aria-hidden="true"></i>
+              <i className="fa fa-wordpress"></i>
+              <i className="fa fa-html5"></i>
+              <i class="fa fa-html5 "></i>
+              <i className="fa fa-wordpress"></i>
+              <i className="fa fa-html5"></i>
+              <i class="fa fa-html5 "></i>
+            </div>
             <div className="skills-text">
               <h3>Skills</h3>
               <p>
@@ -56,20 +66,14 @@ export const About = () => {
                 Lorem Ipsum has been the industry's standard dummy text ever
                 since the 1500s.
               </p>
-            </div>
-            <div className="skills-grid">
-              <i className="fa fa-jsfiddle"></i>
-              <i className="fa fa-css3"></i>
-              <i class="fa fa-github" aria-hidden="true"></i>
-              <i className="fa fa-wordpress"></i>
-              <i className="fa fa-html5"></i>
-              <i class="fa fa-html5 "></i>
+              <div className="about-contact">
+                <button className="btn btn-alternate contact-me">
+                  Contact
+                </button>
+              </div>
             </div>
           </div>
         </Fade>
-        <div className="about-contact">
-          <button className="contact-me">Contact</button>
-        </div>
       </div>
     </div>
   );
