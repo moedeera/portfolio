53b5a33ecde93eebac1d1ca72b95f45d3c81dc53
Moe Deera <deeramoe@gmail.com>
2022-08-16 05:52:14 -0600

    committ

--- src/pages/SingleCase/SIngleCase.js
+++ src/pages/SingleCase/SIngleCase.js
@@ -5,7 +5,14 @@ import { useParams } from "react-router-dom";
 export const SingleCase = () => {
   let { id } = useParams();
 
-  const [accordion, showAccordion] = useState(false);
+  const [accordion, showAccordion] = useState([{id:1, state:true},{id:2, state:false}]);
+
+const toggleAccordion = (accordionId) => {
+
+showAccordion(accordion.map((accord) => accord.id=== accordionId ?{...accord, state:!accord.state} : accord))
+
+}
+
   return (
     <div className="single-case-page">
       <div className="s-case">
@@ -35,7 +42,7 @@ export const SingleCase = () => {
                 <div
                   className="ccb-accordion-box"
                   onClick={() => {
-                    showAccordion(!accordion);
+                    toggleAccordion(1)
                   }}
                 >
                   <div>Statistics at Glance</div>
@@ -45,8 +52,50 @@ export const SingleCase = () => {
                 </div>
 
                 <div
-                  className="ccb-accordion-content"
-                  style={accordion ? { height: "auto" } : { height: "0px" }}
+                  className={accordion[0].state ?"ccb-accordion-content":"ccb-accordion-content hide"}
+
+                 
+                >
+                  <div className="ccb-accordion-summary-box">
+<div className="ccb-accordion-summary">
+  <div className="ccbas-main">Estimated new cases</div><div className="ccbas-main">99,870</div>
+  <div className="ccbas-main">% of All new cancer cases </div><div className="ccbas-main">99,870</div>
+</div>
+
+<div className="ccbas-image">
+  <img src={'https://images.pexels.com/photos/4226264/pexels-photo-4226264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} alt=""/>
+</div>
+</div>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+              </div>
+
+              <div className="ccb-left-accordion">
+                <div
+                  className="ccb-accordion-box"
+                  onClick={() => {
+                    toggleAccordion(2)
+                  }}  
+                                       
+                           >
+                  <div>Statistics at Glance</div>
+                  <div>
+                    <i className="fa fa-plus"></i>
+                  </div>
+                </div>
+
+                <div
+                 
+                  className={accordion[1].state ?"ccb-accordion-content":"ccb-accordion-content hide"}    
                 >
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
@@ -60,6 +109,9 @@ export const SingleCase = () => {
                   like Aldus PageMaker including versions of Lorem Ipsum.
                 </div>
               </div>
+
+
+
               <div className="ccb-left-accordion"></div>
               <div className="ccb-left-accordion"></div>
             </div>
