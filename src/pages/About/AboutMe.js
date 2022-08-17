eebb617fee4271c8d3d0d5195d6dab49356e19d7
Moe Deera <deeramoe@gmail.com>
2022-08-17 03:03:30 -0600

    committ

--- src/pages/About/AboutMe.js
+++ src/pages/About/AboutMe.js
@@ -13,14 +13,13 @@ export const AboutMe = () => {
   </div>
 <div className="upper-abm">
 <div className="upper-abm-picture">
-<h3>
-              If you look into my Github page you understand that Web
-              Development is not just a hobby or a job for me, It's my passion.
-</h3>
+<h1>
+              Web Development is not just a hobby or a job for me, It's my passion.
+</h1>
 </div>
 <div className="upper-abm-text">
   <div className="abm-text-header">
-  <h1>'A Self taught developer that understands it's about{" "}
+  <h1>'Being a developer is about{" "}
               <span
                 style={{
                   color: "red",
@@ -31,19 +30,18 @@ export const AboutMe = () => {
                   what you know{" "}
                 </span>
               </span>
-              <span> what can you solve'</span>
+              <span> what you solve'</span>
  </h1>
   </div>
   <div className="abm-text-summary">
-    <div><p>       My name is Moe and I decided to learn coding in 2020 and
+    <div><p>  Hello there! My name is Moe. I started learning coding back in 2020 and
               immediately fell in love with it. I chose early on that I wanted
-              to focus on Web Development with an emphasis on web design. Unable
-              to attend school due to working full time, I dedicated a minimum
-              of 2 hours a day to learning.</p></div>
-    <div> After looking into the market a little more deeply, I realized
+              to focus on Web Development with an emphasis on Design. Unable
+              to attend school due to working full time, I dedicated 
+              of 2-4 hours a day to learning. </p></div>
+             <div> After looking into the market a little more deeply, I realized
               their was a lot of demand for design/brand oriented websites so I
-              leaned WordPress, WebFlow and Figma. to boost my design skills. I
-              am still learning but have made huge strides since then. My goal
+              leaned WordPress, WebFlow and Figma to boost my design skills. My goal
               is to establish a career in the web Development field and provide
               reliable quality websites.</div>          
   </div>
