506cd4bc0ee00bf4e22aa8c4f3080e6304188be9
moedeera <82620264+moedeera@users.noreply.github.com>
2022-08-19 06:12:37 -0600

    committ

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -21,7 +21,7 @@ import pic6 from "../Project/greenframe.png";
 import pic7 from "../Project/deeracode-mockup.png";
 const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7];
 
-export const Overview = () => {
+export const Overview = ({ page }) => {
   const { projects } = useContext(SiteContext);
   return (
     <div className="overview-page" id="projects">
