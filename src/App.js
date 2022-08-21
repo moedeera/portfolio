5299ae74bf10a00ea5ae44be152e69b38ff36c77
Moe Deera <deeramoe@gmail.com>
2022-08-20 22:43:12 -0600

    committ

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/homepage/Homepage";
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
-import { PortfolioPage } from "./pages/Portfolio/Portfolio";
+import {Admin} from"./pages/admin/Admin"
 import { Services } from "./pages/Services/Services";
 import { Project } from "./pages/Project/Project";
 import { About } from "./pages/About/About";
