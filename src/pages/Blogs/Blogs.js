54f2983c7f6a300185827003f3b5a36c113b3cec
Moe Deera <deeramoe@gmail.com>
2022-08-23 03:27:41 -0600

    comimtt

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -11,13 +11,13 @@ export const Blogs = () => {
         <div className="blogs-upper-section">
           <div
             className="blogs-headliner"
-            style={{ backgroundImage: `url(${articlesList[0].pic})` }}
+            style={{ backgroundImage: `url(${articlesList[3].pic})` }}
           >
             <div className="blogs-overlay">
               <div className="blogs-container">
-                <small>{articlesList[0].topic}</small>
+                <small className="blogs-topic">{articlesList[0].topic}</small>
                 <div>
-                  <div>
+                  <div className="blogs-header">
                     <h3>{articlesList[0].header}</h3>
                   </div>
                   <div className="blogs-headline-details">
@@ -34,23 +34,66 @@ export const Blogs = () => {
                     </small>
                   </div>
                 </div>
-                <small>{articlesList[0].abstract}</small>
+                <small>{articlesList[0].abstract}...</small>
               </div>
             </div>
           </div>
-          <div className="blogs-features">
-            <div className="blogs-features-item">
-              <div className="bf-image">
-                <img
-                  src={articlesList[1].pic}
-                  alt=""
-                  style={{ width: "100px" }}
-                />
+          <div>
+            <h3 className="bf-header">See Also</h3>
+            <div className="blogs-features">
+              <div className="blogs-features-item">
+                <div className="bf-image">
+                  <img
+                    src={articlesList[1].pic}
+                    alt=""
+                    // style={{ width: "100px" }}
+                  />
+                </div>
+                <div className="bf-item">
+                  <small className="blogs-topic-bf">
+                    {articlesList[1].topic}
+                  </small>
+                  <div className="bf-item-header">{articlesList[1].header}</div>
+                  <small div className="bf-item-date">
+                    {articlesList[1].created}
+                  </small>
+                </div>
               </div>
-              <div className="bf-item">
-                <small>{articlesList[1].topic}</small>
-                <h3>{articlesList[1].header}</h3>
-                <small>{articlesList[1].created}</small>
+              <div className="blogs-features-item">
+                <div className="bf-image">
+                  <img
+                    src={articlesList[1].pic}
+                    alt=""
+                    // style={{ width: "100px" }}
+                  />
+                </div>
+                <div className="bf-item">
+                  <small className="blogs-topic-bf">
+                    {articlesList[1].topic}
+                  </small>
+                  <div className="bf-item-header">{articlesList[1].header}</div>
+                  <small div className="bf-item-date">
+                    {articlesList[1].created}
+                  </small>
+                </div>
+              </div>
+              <div className="blogs-features-item">
+                <div className="bf-image">
+                  <img
+                    src={articlesList[1].pic}
+                    alt=""
+                    // style={{ width: "100px" }}
+                  />
+                </div>
+                <div className="bf-item">
+                  <small className="blogs-topic-bf">
+                    {articlesList[1].topic}
+                  </small>
+                  <div className="bf-item-header">{articlesList[1].header}</div>
+                  <small div className="bf-item-date">
+                    {articlesList[1].created}
+                  </small>
+                </div>
               </div>
             </div>
           </div>
