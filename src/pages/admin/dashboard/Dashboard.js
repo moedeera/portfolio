24237c1f496d32fbb75283499c94630930e6afbb
moedeera <82620264+moedeera@users.noreply.github.com>
2022-08-25 00:30:43 -0600

    committ

--- src/pages/admin/dashboard/Dashboard.js
+++ src/pages/admin/dashboard/Dashboard.js
@@ -8,29 +8,29 @@ export const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <div className="dashboard-main">
-        <div className="dashboard-button">
-          {show ? (
-            <>
-              <i
-                className="fas fa-times"
-                onClick={() => {
-                  toggleShow(false);
-                }}
-              ></i>
-            </>
-          ) : (
-            <>
-              <i
-                className="fas fa-bars"
-                onClick={() => {
-                  toggleShow(true);
-                }}
-              ></i>
-            </>
-          )}
-        </div>
         <div className="dashboard-header-block">
-          <div>
+          <div className="dashboard-header-name">
+            <div className="dashboard-button">
+              {show ? (
+                <>
+                  <i
+                    className="fas fa-times"
+                    onClick={() => {
+                      toggleShow(false);
+                    }}
+                  ></i>
+                </>
+              ) : (
+                <>
+                  <i
+                    className="fas fa-bars"
+                    onClick={() => {
+                      toggleShow(true);
+                    }}
+                  ></i>
+                </>
+              )}
+            </div>
             <h3>Hi John !</h3>
           </div>
           <div className="progress-bar-container">
