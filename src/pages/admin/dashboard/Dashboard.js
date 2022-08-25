3a971643aee6efc2559e30f1aa182046fa218fb2
moedeera <82620264+moedeera@users.noreply.github.com>
2022-08-25 00:02:20 -0600

    committ

--- src/pages/admin/dashboard/Dashboard.js
+++ src/pages/admin/dashboard/Dashboard.js
@@ -6,32 +6,63 @@ import "./Dashboard.css";
 export const Dashboard = () => {
   const { toggleShow, show } = useContext(SiteContext);
   return (
-    <div className="admin-page">
-      <div className="dashboard-container">
-        <div className="dashboard-">
-          <div className="dashboard-button">
-            {show ? (
-              <>
-                <i
-                  className="fas fa-times"
-                  onClick={() => {
-                    toggleShow(false);
-                  }}
-                ></i>
-              </>
-            ) : (
-              <>
-                <i
-                  className="fas fa-bars"
-                  onClick={() => {
-                    toggleShow(true);
-                  }}
-                ></i>
-              </>
-            )}
-          </div>
-          Profile
+    <div className="dashboard-container">
+      <div className="dashboard-main">
+        <div className="dashboard-button">
+          {show ? (
+            <>
+              <i
+                className="fas fa-times"
+                onClick={() => {
+                  toggleShow(false);
+                }}
+              ></i>
+            </>
+          ) : (
+            <>
+              <i
+                className="fas fa-bars"
+                onClick={() => {
+                  toggleShow(true);
+                }}
+              ></i>
+            </>
+          )}
         </div>
+        <div className="dashboard-header-block">
+          <div>
+            <h3>Hi John !</h3>
+          </div>
+          <div className="progress-bar-container">
+            {" "}
+            <small>15/30</small>
+            <div className="progress-bar">
+              <div className="dash-progress"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="upper-block">
+          <div className="single-block" id="messages">
+            Messages
+          </div>
+          <div className="single-block" id="cases">
+            Cases
+          </div>
+          <div className="single-block" id="requests">
+            Requests
+          </div>
+          <div className="single-block" id="articles">
+            Articles
+          </div>
+        </div>
+        <div className="lower-block">Lowe Hello</div>
+      </div>
+
+      <div className="dashboard-side">
+        <div className="upper">Upper</div>
+        <div className="mid-level">Midlevel</div>
+        <div className="lower">Lower</div>
       </div>
     </div>
   );
