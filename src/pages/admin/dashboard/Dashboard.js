575da1075e3c642cd7911bf4151782554f28480d
moedeera <82620264+moedeera@users.noreply.github.com>
2022-08-26 01:12:29 -0600

    committ

--- src/pages/admin/dashboard/Dashboard.js
+++ src/pages/admin/dashboard/Dashboard.js
@@ -4,6 +4,9 @@ import "../Admin.css";
 import "./Dashboard.css";
 import messages from "./icons/messages.png";
 import notification from "./icons/notification.png";
+import cases from "./icons/case.png";
+import request from "./icons/request.png";
+// import case from "../../../components/SideMenu/cases.png";
 
 export const Dashboard = () => {
   const { toggleShow, show } = useContext(SiteContext);
@@ -80,14 +83,76 @@ export const Dashboard = () => {
                 </div>
               </div>
             </div>
+
             <div className="single-block" id="cases">
-              Cases
+              <div className="curve second-curve"></div>
+              <div className="content">
+                {" "}
+                <div className="content-icons">
+                  <div>
+                    <img src={cases} alt="logo" />
+                  </div>
+                  <div>
+                    <img src={notification} alt="logo" />
+                  </div>
+                </div>
+                <div className="content-message">4 Unpublished Cases</div>
+                <div className="content-avatars">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"
+                    alt="avatar-pic"
+                  />
+                  <img
+                    src="https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"
+                    alt="avatar-pic"
+                  />
+                </div>
+              </div>
             </div>
+
             <div className="single-block" id="requests">
-              Requests
+              <div className="curve third-curve"></div>
+              <div className="content">
+                {" "}
+                <div className="content-icons">
+                  <div>
+                    <img src={request} alt="logo" />
+                  </div>
+                  <div>
+                    <img src={notification} alt="logo" />
+                  </div>
+                </div>
+                <div className="content-message">4 Pending Requests</div>
+                <div className="content-avatars">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"
+                    alt="avatar-pic"
+                  />
+                  <img
+                    src="https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"
+                    alt="avatar-pic"
+                  />
+                </div>
+              </div>
             </div>
+
             <div className="single-block" id="articles">
-              Articles
+              <div className="curve fourth-curve"></div>
+              <div className="content">
+                {" "}
+                <div className="content-icons"></div>
+                <div className="content-message">5 Articles</div>
+                <div className="content-avatars">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"
+                    alt="avatar-pic"
+                  />
+                  <img
+                    src="https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_960_720.jpg"
+                    alt="avatar-pic"
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <div className="lower-block">Lowe Hello</div>
