ce2572f80f89bb3445e2d2d436c2049e960aeac6
moedeera <82620264+moedeera@users.noreply.github.com>
2022-12-08 07:19:43 -0600

    committ

--- src/pages/LinksPage/LinksPage.js
+++ src/pages/LinksPage/LinksPage.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./LinksPage.css";
 import { Neighborhoods, linkOpener } from "../../assets/data/dataFunction";
+import { useState } from "react";
 
 export const LinksPage = () => {
+  const [links, setLinks] = useState();
+  const [links2, setLinks2] = useState();
+  const [links3, setLinks3] = useState();
+  const [links4, setLinks4] = useState();
+  const [links5, setLinks5] = useState();
+
   const copyTextToClipboard = async (text) => {
     if ("clipboard" in navigator) {
       return await navigator.clipboard.writeText(text);
@@ -16,6 +23,7 @@ export const LinksPage = () => {
       {Neighborhoods.map((hood) =>
         hood.map((area) => (
           <>
+            <h3>{area.area}</h3>
             <div className="link-unit">{`${linkOpener}${area.link}`}</div>
             <button
               className="btn btn-primary"
