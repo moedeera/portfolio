e407a3466afa47c8a43d2ab4e35b9115bee17c2f
moedeera <82620264+moedeera@users.noreply.github.com>
2023-07-05 06:43:39 -0600

    fix commit

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Portfolio.css";
-import MetaTags from "react-meta-tags";
+// import MetaTags from "react-meta-tags";
 
 import { Projects } from "../../components/Projects/Projects";
 import { Overview } from "../Overview/Overview";
@@ -8,14 +8,14 @@ import { Overview } from "../Overview/Overview";
 export const PortfolioPage = () => {
   return (
     <div>
-      <MetaTags>
+      {/* <MetaTags>
         <title>Projects Page</title>
         <meta name="description" content="Projects." />
         <meta property="og:title" content="My Projects" />
         <meta property="og:image" content="path/to/image.jpg" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content="My works" />
-      </MetaTags>
+      </MetaTags> */}
       <div className="portfolio-page-container">
         <h1 style={{ color: "rgb(37, 110, 255)" }}>Portfolio </h1>
         <div>
